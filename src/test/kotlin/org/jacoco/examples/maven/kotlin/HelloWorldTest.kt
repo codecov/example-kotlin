3d488dd7b0154e79723179028f571f71aac44dd3
stevepeak <steve@stevepeak.net>
2014-09-28 19:04:15 +0000

    initial commit

---
+++ src/test/kotlin/org/jacoco/examples/maven/kotlin/HelloWorldTest.kt
@@ -0,0 +1,12 @@
+package org.jacoco.examples.maven.kotlin
+
+import junit.framework.TestCase
+import kotlin.test.assertEquals
+
+class HelloWorldTest : TestCase() {
+
+	fun testAssert() : Unit {
+		assertEquals("Hello World!", HelloWorld().getMessage(false))
+	}
+
+}
